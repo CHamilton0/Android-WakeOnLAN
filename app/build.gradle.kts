b6f7810a181f6c62f8f97927f1d31b91e5fb2df5
Christopher Hamilton <chrish001@live.com.au>
2025-12-08 20:45:58 +1030

    fix: build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.release
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
