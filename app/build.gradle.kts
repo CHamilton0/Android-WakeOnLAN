125efcd1994ad0f110bb8743b0dd03a41cb04637
Version CI <chamilton0@users.noreply.github.com>
2025-12-08 09:13:57 +0000

    ci(version): updates version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.privateparking.wakeonlan"
         minSdk = 33
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
